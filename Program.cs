5799a92c2db90dd6acbda5247abf2b820f7f58a4
DeniseBarbosa <deniseluvoir@gmail.com>
2022-12-12 09:38:39 -0300

    Dapper Contrib

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Blog.Modelo;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog{
+    class Program{
+
+
+        private const string stringConeccao = "Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
+
+        static void Main(string[]args){
+            LerUsuarios();
+        }
+
+        public static void LerUsuarios(){
+
+            using(var coneccao = new SqlConnection(stringConeccao)){
+                
+                var usuarios = coneccao.GetAll<Usuario>();//listar todos os meus usuario na base de dados
+                foreach(var usuario in usuarios){
+                    Console.WriteLine(usuario.Nome);
+                }
+            }
+
+        }
+    }
+}
